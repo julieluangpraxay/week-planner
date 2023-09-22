e42a700e2eb6634eef8179ac83d97432bc6437fd
Thomas Nguyen <tomxnguy@gmail.com>
2023-09-22 22:05:45 +0000

    fixed modal buttons

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const $button = document.querySelector('.open-modal');
 const $modal = document.querySelector('.container-modal');
 
 $button.addEventListener('click', function (event) {
+  console.log('button', $button);
   $modal.classList.remove('hidden');
 });
 
@@ -12,11 +13,4 @@ const $grayBackground = document.querySelector('.gray-background');
 $openModal.addEventListener('click', function () {
   $popup.classList.remove('hidden');
   $grayBackground.classList.remove('hidden');
-});
-
-const $noButton = document.querySelector('.no-button');
-
-$noButton.addEventListener('click', function () {
-  $popup.classList.add('hidden');
-  $grayBackground.classList.add('hidden');
 });
