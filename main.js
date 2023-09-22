b2a389db65cdcb22ed4ec879b754a0f115ced65a
julieluangpraxay <julieluangpraxay@gmail.com>
2023-09-22 23:03:50 +0000

    WIP

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ const $button = document.querySelector('.open-modal');
 const $modal = document.querySelector('.container-modal');
 
 $button.addEventListener('click', function (event) {
-  console.log('button', $button);
   $modal.classList.remove('hidden');
 });
 
@@ -13,4 +12,24 @@ const $grayBackground = document.querySelector('.gray-background');
 $openModal.addEventListener('click', function () {
   $popup.classList.remove('hidden');
   $grayBackground.classList.remove('hidden');
+});
+
+const $form = document.querySelector('form');
+const $input = document.querySelector('input');
+const $time = document.querySelector('.time-event');
+const $day = document.querySelectorAll('.form-select');
+const $eventInfo = document.querySelector('.event-info');
+
+$form.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const time = $form.elements.time.value;
+  const day = $form.elements.day.value;
+  const eventInfo = $form.elements.eventInfo.value;
+
+  const tableData = {
+    time: [],
+    day: [],
+    eventInfo: []
+  };
 });
