e71468829a1b4e3704be91ba90e6eeb22cf99a04
julieluangpraxay <julieluangpraxay@gmail.com>
2023-09-22 21:16:20 +0000

    Added modal

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
-const $button = document.querySelector('button');
+const $button = document.querySelector('.open-modal');
+const $modal = document.querySelector('.container-modal');
 
 $button.addEventListener('click', function (event) {
+  $modal.classList.remove('hidden');
+});
 
+const $openModal = document.querySelector('.open-modal');
+const $popup = document.querySelector('.popup');
+const $grayBackground = document.querySelector('.gray-background');
+
+$openModal.addEventListener('click', function () {
+  $popup.classList.remove('hidden');
+  $grayBackground.classList.remove('hidden');
+});
+
+const $noButton = document.querySelector('.no-button');
+
+$noButton.addEventListener('click', function () {
+  $popup.classList.add('hidden');
+  $grayBackground.classList.add('hidden');
 });
